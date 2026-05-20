08243dcd57792e7b9e97554c84186c1053d6f409
Kennijy <150828844+Kennnijy@users.noreply.github.com>
2026-05-20 23:48:26 +0800

    Update run_pipeline.sh

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,6 +1,8 @@
-cd final/Compiler-sample/
-
 #!/bin/bash
+# 修正：#!/bin/bash 必須放在檔案的最頂端第一行
+
+# 切換到專案資料夾
+cd ~/final/Compiler-sample/
 
 # 只限定測試固定檔名 sample5
 FILE_NAME="sample5"
@@ -18,7 +20,6 @@ else
     exit 1
 fi
 
-
 # 2. 安裝 LLVM 與 Clang 後端工具
 echo "[步驟 2/8] 檢查並安裝 Clang 與 LLVM..."
 sudo apt-get install clang llvm -y
@@ -32,9 +33,9 @@ llc --version
 echo "[步驟 4/8] 正在將 C 碼轉為 LLVM IR 中間碼..."
 clang -S -emit-llvm "${FILE_NAME}.c"
 
-# 5. 進行 LLVM 最佳化 (已修正為新版 opt 的 -passes 語法)
+# 5. 進行 LLVM 最佳化 (修正：配合 Ubuntu 18.04 的 LLVM 6.0 舊版 opt 語法)
 echo "[步驟 5/8] 正在使用 opt 進行 mem2reg 最佳化..."
-opt -S -passes='globalopt,loop-simplify,mem2reg' "${FILE_NAME}.ll" -o "${FILE_NAME}_opt.ll"
+opt -S -globalopt -loop-simplify -mem2reg "${FILE_NAME}.ll" -o "${FILE_NAME}_opt.ll"
 
 # 6. 將最佳化後的中間碼轉為組合語言 (.s)
 echo "[步驟 6/8] 正在使用 llc 將中間碼轉為 x86 組合語言..."
