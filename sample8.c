53de4930305ecbf8b9416d57bb4822e9a6fb118e
Kennijy <150828844+Kennnijy@users.noreply.github.com>
2026-05-20 22:53:52 +0800

    Update sample8.c

--- sample8.c
+++ sample8.c
@@ -4,7 +4,7 @@ int main() {
     int n;
     int steps;
     
-    n = 26;
+    n = 27;
     steps = 0;
 
     while (n != 1) {
@@ -18,21 +18,3 @@ int main() {
     printf("%d\n", steps);
     return 0;
 }
-
-
-
-
-bison                              (yyparse)
-  bas.y ─────────────────► [ yacc ] ─────────────────────► y.tab.c ──────┐
-                             │                                           │
-                             ▼ (產生標頭檔)                               │
-                          y.tab.h                                        │
-                             │                                           │
-                             ▼ (引入定義)                                 ▼
-  bas.l ─────────────────► [ lex ] ──────────────────────► lex.yy.c ────► [ cc ] ──► Compile
-               flex                               (yylex)                │
-                                                                         │
- ┌───────────────────────────────────────────────────────────────────────┘
- │ (測試原始碼)
- ▼
-source.c ──► [ ./Compile ] ──► (語法檢查) ──► [ Clang/LLVM ] ──► [ ld ] ──► 可執行檔 (Output)
